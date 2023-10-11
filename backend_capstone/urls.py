c834590fbadc5d391ad1bb4083ebc47187c6ac31
Abdulaziz Sayed <hanafiibrahim87@yahoo.com>
2023-10-11 21:09:29 +0300

    Added `api` prefix for menu and booking APIs

--- backend_capstone/urls.py
+++ backend_capstone/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("restaurant/", include("Restaurant.urls")),
+    path("api/restaurant/", include("Restaurant.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
 ]
